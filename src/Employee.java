e39ea184c20d72ae6cdc16931c3c813a30b8c4dd
Yogesh Tonape <tonapeyogesh@rediffmail.com>
2019-05-14 14:40:51 +0530

    First commit

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+	private static String name;
+	public static void main(String[] args) {
+		 name="Umesh";
+		 display();
+			
+	}
+	public static void display()
+	{
+		System.out.println(name);
+	}
+}
